bbcbc0388325c115d21afed72c65c57954f08c4e
Samanta_Windows <samanta.ortuno@gmail.com>
2025-01-05 18:53:29 +0100

    Update of the code

--- js/app.js
+++ js/app.js
@@ -1,49 +1,178 @@
 // Variables
-/* const formulario = document.querySelector('#contenido-principal') */
-let arrayGastos = [];
+const addExpenses = document.querySelector('.list-group')
+const btnAdd=document.querySelector('button[type="submit"]')
 
 // Events
 eventListeners();
 function eventListeners(){
     document.addEventListener('DOMContentLoaded', validateAnswer);
-    document.querySelector('button[type="submit"]').addEventListener('click',readValue);
+    btnAdd.addEventListener('click',addList);
 }
 
 // Classes
-class Calculos {
-    constructor (restante,cantidad) {
-        this.restante = restante;
-        this.cantidad = cantidad;
+class Calc {
+    constructor (expenses) {
+        this.expenses = Number(expenses);
+        this.rest = Number(expenses);
+        this.arrayExpenses = [];
     }
 
     // Methods
+    addExpensesList (object){
+        this.arrayExpenses=[...this.arrayExpenses,object];      
+        this.setRest();
+    }
 
+    setRest (){
+        const finalTotal = this.arrayExpenses.reduce( (total, expense) => {
+            return total + expense.quantity;
+        },0)
+        this.rest = this.expenses - finalTotal;        
+    }
+
+    removeExpense (id) {
+        this.arrayExpenses = this.arrayExpenses.filter((expense) => {
+            expense.id !== id
+        });
+        ui.printList(this.arrayExpenses);
+
+        ui.printNewRest(this.rest);
+
+        ui.checkExpenses(this)
+    }
+
+}
+
+class UI {
+    insertarDatos (quantity) {
+        const {expenses, rest} = quantity;
+        document.querySelector('#total').textContent = expenses;
+        document.querySelector('#restante').textContent = rest;
+    }
+    
+    printAlert (message,type){
+        const br = document.createElement('br');
+        const elementMessage=document.createElement('DIV')
+        elementMessage.classList.add('text-center', 'alert');
+
+        if (type==='error') {
+            elementMessage.classList.add('alert-danger');
+        } else {
+            elementMessage.classList.add('alert-success');
+        }
+
+        elementMessage.textContent=message;
+        document.querySelector('.primario').appendChild(br)
+        document.querySelector('.primario').appendChild(elementMessage);
+        
+        setTimeout(() => {
+            br.remove();
+            elementMessage.remove();
+        }, 3000);
+    }
+    
+    printList (array) {
+        clearHTML();
+        array.forEach((item) => {
+            const element = document.createElement('LI');
+            element.className = 'list-group-item d-flex justify-content-between align-items-center';
+            const {id, expense, quantity} = item;
+            element.dataset.id=id;
+            element.textContent = `
+                ${expense}: $ ${quantity} 
+                    `
+
+            const button = document.createElement('BUTTON');
+            button.classList.add('btn','btn-danger','borrar-expense');
+            button.textContent='Borrar X';
+            button.onclick = () => {
+                finalExpenses.removeExpense(id);
+            };
+
+
+            element.appendChild(button);
+            addExpenses.appendChild(element);     
+        })
+    }
+
+    printNewRest (rest) {
+        document.querySelector('#restante').textContent = rest;
+    }
+
+    checkExpenses(expensesObj){
+        const {expenses, rest} = expensesObj;
+        const classRest = document.querySelector('.restante');
+
+        if(rest < (0.75*expenses)){
+            classRest.classList.remove('alert-success', 'alert-warning')
+            classRest.classList.add('alert-danger')
+            
+        } else if (rest < (0.5*expenses)) {
+            classRest.classList.remove('alert-success', 'alert-danger')
+            classRest.classList.add('alert-warning')
+        } else {
+            classRest.classList.remove('alert-warning','alert-danger')
+            classRest.classList.add('alert-success')
+        }
+
+        if (rest <= 0){
+            ui.printAlert('There is no money left','error')
+            btnAdd.disabled=true;
+        }
+
+
+    }
 
 
 
 }
+
+const ui = new UI();
+let finalExpenses;
 
 // Functions
 function validateAnswer(){ // Validate the prompt answer
-    let answer = Number(prompt ('¿Cuánto es el presupuesto?'));
-    const restante = document.querySelector('.restante span');
-    const presupuesto = document.querySelector('.presupuesto span');
+    
+    let answer = Number(prompt ('How much is the budget?'));
 
-    if (answer == 0 || isNaN(answer)) {
+    if (answer <= 0 || isNaN(answer)) {
         window.location.reload()
         return;
     } 
-    presupuesto.textContent = answer;
-    restante.textContent = answer;
+    finalExpenses = new Calc(answer)
+    ui.insertarDatos(finalExpenses)
+}
+
+function addList (e) {
+    e.preventDefault();
+    
+    const id = Math.random();
+    const expense = document.querySelector('input#gasto').value;
+    const quantity = Number(document.querySelector('input#cantidad').value);
+
+    if (expense === '' || quantity === ''){
+        ui.printAlert("Ambos campos son obligatorios", 'error');
+    } else if (quantity <= 0 || isNaN(quantity)) {
+        ui.printAlert("La quantity debe ser un NÚMERO mayor a 0", 'error');
+    } else {
+        ui.printAlert("Gasto agregado", 'correcto'); 
+    }
+
+    const object = { id, expense, quantity }   
+    finalExpenses.addExpensesList(object);    
+
+    ui.printList(finalExpenses.arrayExpenses);
+    ui.printNewRest(finalExpenses.rest)
+
+    ui.checkExpenses(finalExpenses)
+}
+
+function clearHTML(){
+    while(addExpenses.firstChild){
+        addExpenses.removeChild(addExpenses.firstChild)
+    }
 }
 
 
 
-function readValue(e) { // Read the cost that we agregate
-    e.preventDefault(); // Prevent the default action of the button
-    const gasto = document.querySelector('input#gasto').value;
-    const cantidad = document.querySelector('input#cantidad').value;
-
-    
-    console.log(gasto,cantidad);
-};
+/* > < */
